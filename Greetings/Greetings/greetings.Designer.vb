36e48accfd5fc2feb3ca167c304efe8a4daf2187
JLAW7 <60242914+JLAW7@users.noreply.github.com>
2020-02-06 20:26:00 -0500

    printed greetings to listbox and added picture box
    Used listbox to print value of txtName to lstOut. Added clear and exit buttons. Added picGreetings.

--- Greetings/Greetings/greetings.Designer.vb
+++ Greetings/Greetings/greetings.Designer.vb
@@ -22,11 +22,16 @@ Partial Class Greetings
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Greetings))
         Me.btnClick = New System.Windows.Forms.Button()
         Me.txtName = New System.Windows.Forms.TextBox()
         Me.lblHeading = New System.Windows.Forms.Label()
         Me.lblName = New System.Windows.Forms.Label()
-        Me.txtOut = New System.Windows.Forms.TextBox()
+        Me.lstOut = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btnClick
@@ -34,9 +39,9 @@ Partial Class Greetings
         Me.btnClick.BackColor = System.Drawing.Color.Blue
         Me.btnClick.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnClick.ForeColor = System.Drawing.Color.White
-        Me.btnClick.Location = New System.Drawing.Point(206, 240)
+        Me.btnClick.Location = New System.Drawing.Point(50, 225)
         Me.btnClick.Name = "btnClick"
-        Me.btnClick.Size = New System.Drawing.Size(175, 52)
+        Me.btnClick.Size = New System.Drawing.Size(181, 52)
         Me.btnClick.TabIndex = 0
         Me.btnClick.Text = "Click Me"
         Me.btnClick.UseVisualStyleBackColor = False
@@ -47,9 +52,9 @@ Partial Class Greetings
         Me.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.txtName.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtName.ForeColor = System.Drawing.Color.Black
-        Me.txtName.Location = New System.Drawing.Point(207, 181)
+        Me.txtName.Location = New System.Drawing.Point(50, 166)
         Me.txtName.Name = "txtName"
-        Me.txtName.Size = New System.Drawing.Size(174, 29)
+        Me.txtName.Size = New System.Drawing.Size(181, 29)
         Me.txtName.TabIndex = 1
         Me.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
@@ -72,21 +77,57 @@ Partial Class Greetings
         Me.lblName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.lblName.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblName.ForeColor = System.Drawing.Color.White
-        Me.lblName.Location = New System.Drawing.Point(204, 146)
+        Me.lblName.Location = New System.Drawing.Point(50, 106)
         Me.lblName.Name = "lblName"
         Me.lblName.Size = New System.Drawing.Size(181, 23)
         Me.lblName.TabIndex = 3
         Me.lblName.Text = "Enter Your Name"
         '
-        'txtOut
+        'lstOut
         '
-        Me.txtOut.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtOut.Location = New System.Drawing.Point(167, 319)
-        Me.txtOut.Multiline = True
-        Me.txtOut.Name = "txtOut"
-        Me.txtOut.Size = New System.Drawing.Size(251, 75)
-        Me.txtOut.TabIndex = 4
-        Me.txtOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.lstOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lstOut.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstOut.FormattingEnabled = True
+        Me.lstOut.ItemHeight = 21
+        Me.lstOut.Location = New System.Drawing.Point(21, 310)
+        Me.lstOut.Name = "lstOut"
+        Me.lstOut.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lstOut.Size = New System.Drawing.Size(238, 86)
+        Me.lstOut.TabIndex = 4
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Blue
+        Me.btnClear.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.White
+        Me.btnClear.Location = New System.Drawing.Point(1, 429)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(118, 36)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Blue
+        Me.btnExit.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(148, 429)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(121, 36)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(275, 63)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(318, 402)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
         '
         'Greetings
         '
@@ -94,13 +135,17 @@ Partial Class Greetings
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.LightBlue
         Me.ClientSize = New System.Drawing.Size(605, 512)
-        Me.Controls.Add(Me.txtOut)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstOut)
         Me.Controls.Add(Me.lblName)
         Me.Controls.Add(Me.lblHeading)
         Me.Controls.Add(Me.txtName)
         Me.Controls.Add(Me.btnClick)
         Me.Name = "Greetings"
         Me.Text = "Greetings"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -110,5 +155,8 @@ Partial Class Greetings
     Friend WithEvents txtName As TextBox
     Friend WithEvents lblHeading As Label
     Friend WithEvents lblName As Label
-    Friend WithEvents txtOut As TextBox
+    Friend WithEvents lstOut As ListBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
